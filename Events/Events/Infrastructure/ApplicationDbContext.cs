1945e1b63cd784d170d5628b4aa0f0ce6633ae20
Kirill Melentyev <melentyev.k@gmail.com>
2014-10-13 21:19:17 +0400

    logging db

--- Events/Events/Infrastructure/ApplicationDbContext.cs
+++ Events/Events/Infrastructure/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Events.Models;
+using System.IO;
 
 namespace Events.Infrastructure
 {
@@ -13,8 +14,14 @@ namespace Events.Infrastructure
         public ApplicationDbContext()
             : base("DefaultConnection")
         {
+            Database.Log = (msg => {
+                //myLog.Add(msg);
+                myFileLog.Write(msg);
+            });
         }
-
+        static StreamWriter myFileLog = new StreamWriter(new FileStream(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\mydblog.txt", FileMode.Append));
+        static IList<string> myLog = new List<string>();
         public System.Data.Entity.DbSet<Events.Models.Event> Events { get; set; }
     }
 }
