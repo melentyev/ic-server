50b82f7e2871a9255d3d193469e1d0395e56d4d7
Kirill Melentyev <melentyev.k@gmail.com>
2015-03-21 20:40:07 +0300

    abstract data repo

--- Events/Events/Abstract/IDataRepository.cs
+++ Events/Events/Abstract/IDataRepository.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 using Events.Models;
 
@@ -10,9 +13,13 @@ namespace Events.Abstract
 {
     public interface IDataRepository
     {
+        Database Database { get; }
         IQueryable<Event> Events { get; }
         IQueryable<Comment> Comments { get; }
         IQueryable<Photo> Photos { get;  }
+        IQueryable<UserFile> UserFiles { get; }
+        IQueryable<ApplicationUser> Users { get; }
         Task<IEnumerable<EventViewModel>> GetEventsWithCommentsAndPhotos(IQueryable<Event> query);
+        Task SaveChangesAsync();
     }
 }
